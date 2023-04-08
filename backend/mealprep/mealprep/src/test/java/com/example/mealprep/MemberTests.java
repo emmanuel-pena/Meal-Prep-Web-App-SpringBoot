6becfbef955a543a39d2c2c26f70dee9ce4abf21
Emmanuel <epena6@ucsc.edu>
2023-04-08 13:46:21 -0700

    MemberJdbcDAO tests

--- backend/mealprep/mealprep/src/test/java/com/example/mealprep/MemberTests.java
+++ backend/mealprep/mealprep/src/test/java/com/example/mealprep/MemberTests.java
@@ -1,40 +1,60 @@
 package com.example.mealprep;
 
+import com.example.mealprep.config.JwtService;
 import com.example.mealprep.dao.MemberJdbcDAO;
 import com.example.mealprep.model.Member;
+import lombok.RequiredArgsConstructor;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = {MemberTests.class})
 public class MemberTests {
-
     @Mock
     JdbcTemplate jdbcTemplate;
 
-    @InjectMocks
-    MemberJdbcDAO memberJdbcDAO;
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    @Before
+    private AuthenticationManager authenticationManager;
+
+    private JwtService jwtService = new JwtService();
+
+    private JavaMailSenderImpl mailSenderImpl = new JavaMailSenderImpl();
+
+    @BeforeEach
     public void setUp() throws Exception {
         MockitoAnnotations.openMocks(this);
     }
 
     @Test
+    @DisplayName("Successfully creates a new valid account.")
+    @Order(1)
     public void test_createNewAccount()
     {
         HashMap<String, String> info = new HashMap<>();
@@ -42,15 +62,25 @@ public class MemberTests {
         info.put("username", "MockitoTest");
         info.put("password", "Zapata101");
 
+       Member member = new Member(info.get("email"), info.get("username"), info.get("password"), false, "native");
+       member.setId(UUID.randomUUID().toString());
 
-        Member member = new Member(info.get("email"), info.get("username"), info.get("password"), false, "native");
+        MemberJdbcDAO memberJdbcDAO = spy(new MemberJdbcDAO(jdbcTemplate, passwordEncoder, authenticationManager, jwtService, mailSenderImpl));
 
-        when(memberJdbcDAO.create(member)).thenReturn(ResponseEntity.status(HttpStatus.OK).build());
+        when(jdbcTemplate.queryForObject(anyString(), any(RowMapper.class))).thenReturn(member);
+
+        when(memberJdbcDAO.userWithUsernameExists(member.getUsername())).thenReturn(false);
+        when(memberJdbcDAO.userWithEmailExists(member.getEmail())).thenReturn(false);
+
+
+        assertEquals(ResponseEntity.status(HttpStatus.OK).build().getStatusCode(), memberJdbcDAO.create(member).getStatusCode());
 
     }
 
     @Test
-    public void test_giveErrorCreatingExistingAccount()
+    @Order(2)
+    @DisplayName("Gives error when trying to create account with existing email.")
+    public void test_attemptCreatingExistingEmailAccount()
     {
         HashMap<String, String> info = new HashMap<>();
         info.put("email", "MockitoTest@gmail.com");
@@ -58,9 +88,40 @@ public class MemberTests {
         info.put("password", "Zapata101");
 
         Member member = new Member(info.get("email"), info.get("username"), info.get("password"), false, "native");
+        member.setId(UUID.randomUUID().toString());
 
-        when(memberJdbcDAO.create(member)).thenReturn(ResponseEntity.status(HttpStatus.OK).build());
+        MemberJdbcDAO memberJdbcDAO = spy(new MemberJdbcDAO(jdbcTemplate, passwordEncoder, authenticationManager, jwtService, mailSenderImpl));
+
+        when(jdbcTemplate.queryForObject(anyString(), any(RowMapper.class))).thenReturn(member);
+
+        when(memberJdbcDAO.userWithUsernameExists(member.getUsername())).thenReturn(false);
+        when(memberJdbcDAO.userWithEmailExists(member.getEmail())).thenReturn(true);
 
 
+        assertEquals(ResponseEntity.status(HttpStatus.CONFLICT).build().getStatusCode(), memberJdbcDAO.create(member).getStatusCode());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("Gives error when trying to create account with existing Username.")
+    public void test_attemptCreatingExistingUsernameAccount()
+    {
+        HashMap<String, String> info = new HashMap<>();
+        info.put("email", "MockitoTest@gmail.com");
+        info.put("username", "MockitoTest");
+        info.put("password", "Zapata101");
+
+        Member member = new Member(info.get("email"), info.get("username"), info.get("password"), false, "native");
+        member.setId(UUID.randomUUID().toString());
+
+        MemberJdbcDAO memberJdbcDAO = spy(new MemberJdbcDAO(jdbcTemplate, passwordEncoder, authenticationManager, jwtService, mailSenderImpl));
+
+        when(jdbcTemplate.queryForObject(anyString(), any(RowMapper.class))).thenReturn(member);
+
+        when(memberJdbcDAO.userWithUsernameExists(member.getUsername())).thenReturn(true);
+        when(memberJdbcDAO.userWithEmailExists(member.getEmail())).thenReturn(false);
+
+
+        assertEquals(ResponseEntity.status(HttpStatus.CONFLICT).build().getStatusCode(), memberJdbcDAO.create(member).getStatusCode());
     }
 }
